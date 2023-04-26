c19d43343e5ee08604fcb39c6331e0b0cd717f35
vivek879174 <126542045+vivek879174@users.noreply.github.com>
2023-04-26 12:42:09 +0530

    Add files via upload

---
+++ to-do-list/data.php
@@ -0,0 +1,17 @@
+<?php
+   $title=$_POST['title'];
+
+   include 'database.php';
+
+   $sql="INSERT INTO todos(title)VALUES('$title')";
+
+   $result=mysqli_query($conn, $sql);
+
+   if($result){
+    header("location: ./index.php");
+   }
+
+
+
+
+?>
